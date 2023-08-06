714fb7b6760fb392441fc49cfc66574880fa2090
Chris <schauer@inf-schauer.de>
2023-08-06 15:27:18 +0200

    initial commit -> list deps of package.json

---
+++ npm_collector/depencencies_extractor.py
@@ -0,0 +1,31 @@
+import json
+
+
+# returns a dict of dependencies where key = package name an value = semantic version
+def list_dependencies(file) -> dict:
+    all_deps = dict()
+    package_json = json.load(file)
+    deps_tag = "dependencies"
+    deps_dev_tag = "devDependencies"
+    deps_peer_tag = "peerDependencies"
+
+    all_deps.update(set_of_deps(deps_tag, package_json))
+    all_deps.update(set_of_deps(deps_dev_tag, package_json))
+    all_deps.update(set_of_deps(deps_peer_tag, package_json))
+    return all_deps
+
+
+def set_of_deps(tag: str, json_object: any) -> set:
+    result = dict()
+    if tag in json_object:
+        for key in json_object[tag]:
+            result[key] = json_object[tag][key]
+    return result
+
+
+if __name__ == "__main__":
+    deps = list_dependencies(open("../test/resources/package_all.json"))
+    for dep in deps:
+        print("npm pack " + dep + "@\"" + deps[dep] + "\"")
+
+
